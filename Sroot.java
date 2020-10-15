28240bd73fb28947443b585fa41bf2f7965c069d
Arpit Raj <56836310+arpitraj5@users.noreply.github.com>
2020-10-15 14:10:13 +0530

    Create Sroot.java

---
+++ Sroot.java
@@ -0,0 +1,11 @@
+import java.lang.Math;
+class Sroot
+{
+	public static void main(String args[])
+	{
+		double a=Duble.parseDouble(args[0]);
+		double b;
+		b=Math.sqrt(a);
+		System.out.println("Square Root of "+args[0]+" is "+b);
+	}
+}
