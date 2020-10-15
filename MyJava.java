157e232a364992e3e2e7059696b60c9c42b10404
codAR-123 <72911901+codAR-123@users.noreply.github.com>
2020-10-15 13:32:58 +0530

    Add files via upload

---
+++ MyJava.java
@@ -0,0 +1,13 @@
+public class MyJava
+{
+public static void main(String args[])
+{
+int A=50;
+int B=20;
+System.out.println(A & B);
+System.out.println(A ^ B);
+System.out.println(A | B);
+System.out.println(A<<2);
+System.out.println(A>>2);
+}
+}
